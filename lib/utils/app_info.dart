39b7005d2516c2bfcc25935c6732245428cff225
Everett406 <Everett406@users.noreply.github.com>
2026-05-23 04:39:10 +0000

    chore: 版本号更新为 1.7.4+80

--- lib/utils/app_info.dart
+++ lib/utils/app_info.dart
@@ -1,7 +1,7 @@
 /// App version info - single source of truth
 /// Update this when releasing a new version
 class AppInfo {
-  static const String version = '1.7.3';
-  static const int buildNumber = 79;
-  static const String fullVersion = 'v1.7.3+79';
+  static const String version = '1.7.4';
+  static const int buildNumber = 80;
+  static const String fullVersion = 'v1.7.4+80';
 }
